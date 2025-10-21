775e08be739aebd999dff2d0c10151c1b2638916
Sriramoju Umesh <umeshsriramoju777@gmail.com>
2025-10-21 11:36:59 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,12 @@
-// Import the functions you need from the SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, query, where, orderBy, onSnapshot, serverTimestamp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// firebase-config.js
+// Exports initialized firebase app objects: auth, db, functions
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-functions.js";
+
+// TODO: Replace the config object below with your Firebase project's config.
 // Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCdda9CT4-7gkwSKAreuu7kgtFyYaFSx5U",
@@ -13,8 +17,10 @@ const firebaseConfig = {
   appId: "1:499814052421:web:fb0ad1f7676927b6aea92c",
   measurementId: "G-10Z45CV842"
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const functions = getFunctions(app);
+
+export { app, auth, db, functions };
+
