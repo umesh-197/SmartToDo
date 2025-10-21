f56acb8c10cf7579213f9a2dbb67d7a121ca03b7
Sriramoju Umesh <umeshsriramoju777@gmail.com>
2025-10-21 12:13:49 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,22 @@
-
 // firebase-config.js
-// Exports initialized firebase app objects: auth, db, functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { getFunctions } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-functions.js";
 
-// TODO: Replace the config object below with your Firebase project's config.
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCdda9CT4-7gkwSKAreuu7kgtFyYaFSx5U",
   authDomain: "todolistweb-2433c.firebaseapp.com",
   projectId: "todolistweb-2433c",
-  storageBucket: "todolistweb-2433c.firebasestorage.app",
+  storageBucket: "todolistweb-2433c.appspot.com",
   messagingSenderId: "499814052421",
   appId: "1:499814052421:web:fb0ad1f7676927b6aea92c",
   measurementId: "G-10Z45CV842"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const functions = getFunctions(app);
 
 export { app, auth, db, functions };
-
